40aa4cf9761361d0a96a80a0946db6238b089867
Walid <walid.boussaboun@alamoconsulting.com>
2019-06-11 18:06:44 +0200

    added new characters

--- src/ModGen.java
+++ src/ModGen.java
@@ -57,7 +57,6 @@ public class ModGen {
 	}
 
 	private static void executeAction(JSONObject instruction) {
-		// TODO Auto-generated method stub
 		JSONObject instructionObject = (JSONObject) instruction.get("instruction");
 		
 		pressKey(getKey(instructionObject.get("key")), (Long) instructionObject.get("duration"), (Long) instructionObject.get("delay"));
@@ -93,8 +92,21 @@ public class ModGen {
 		case"b":return KeyEvent.VK_B;
 		case"n":return KeyEvent.VK_N;
 		case"m":return KeyEvent.VK_M;
+		case"1":return KeyEvent.VK_1;
+		case"2":return KeyEvent.VK_2;
+		case"3":return KeyEvent.VK_3;
+		case"4":return KeyEvent.VK_4;
+		case"5":return KeyEvent.VK_5;
+		case"6":return KeyEvent.VK_6;
+		case"7":return KeyEvent.VK_7;
+		case"8":return KeyEvent.VK_8;
+		case"9":return KeyEvent.VK_9;
+		case"0":return KeyEvent.VK_0;
+		case"space":return KeyEvent.VK_SPACE;
+		case"down":return KeyEvent.VK_DOWN;
+		default:return 0;
 		}
-		return 0;
+		
 	}
 
 	private static void pressKey(int vk, Long duration, Long delay) {
